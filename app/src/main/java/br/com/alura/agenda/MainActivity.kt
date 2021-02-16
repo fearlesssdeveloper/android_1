07ec006fe28e4202ecfdc822e2752706938ecf4d
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-16 08:40:36 -0300

    android_1 - 01 - Criando o primeiro App

---
+++ app/src/main/java/br/com/alura/agenda/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.alura.agenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "José Arlan", Toast.LENGTH_SHORT).show()
+    }
+}
