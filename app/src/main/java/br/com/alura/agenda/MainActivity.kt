93b161766f94cea2a476dadecb7555b602a460f7
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-16 09:15:30 -0300

    android_1 - 02 - Implementando o layout da activity

--- app/src/main/java/br/com/alura/agenda/MainActivity.kt
+++ app/src/main/java/br/com/alura/agenda/MainActivity.kt
@@ -1,13 +1,12 @@
 package br.com.alura.agenda
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "José Arlan", Toast.LENGTH_SHORT).show()
     }
 }
