39cb3ba33292932ce6a1b8f64f707098eb931588
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-16 09:46:01 -0300

    android_1 - 02 - Iniciando a implementação do layout de lista

--- app/src/main/java/br/com/alura/agenda/MainActivity.kt
+++ app/src/main/java/br/com/alura/agenda/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val alunos = mutableListOf("Alex", "Fran", "José")
+        val primeiroAluno = findViewById<TextView>(R.id.textView1)
+        val segundoAluno = findViewById<TextView>(R.id.textView2)
+        val terceiroAluno = findViewById<TextView>(R.id.textView3)
+        primeiroAluno.text = alunos[0]
+        segundoAluno.text = alunos[1]
+        terceiroAluno.text = alunos[2]
     }
 }
