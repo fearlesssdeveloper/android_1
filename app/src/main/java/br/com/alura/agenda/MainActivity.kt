14804f0e80bff402c92671171a1f97d80c2b75d7
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-17 11:51:48 -0300

    android_1 - 04 - Inserindo ícone e App bar

--- app/src/main/java/br/com/alura/agenda/MainActivity.kt
+++ app/src/main/java/br/com/alura/agenda/MainActivity.kt
@@ -3,13 +3,13 @@ package br.com.alura.agenda
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Lista de Alunos"
         val alunos = mutableListOf("Alex", "Fran", "José", "Maria", "Anna")
         val listaDeAlunos = findViewById<ListView>(R.id.activity_main_lista_de_alunos)
         listaDeAlunos.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos)
