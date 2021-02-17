1e59bb9a3ab8ec2f051ccaca8a0c4609b575e47c
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-17 14:12:20 -0300

    android_1 - 04 - Criando a activity via Android Studio

---
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.alura.agenda.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.agenda.R
+
+class FormularioAlunoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_aluno)
+    }
+}
--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.kt
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.kt
@@ -1,17 +1,18 @@
-package br.com.alura.agenda
+package br.com.alura.agenda.ui.activity
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.agenda.R
 
-class MainActivity : AppCompatActivity() {
+class ListaAlunosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lista_alunos)
         title = "Lista de Alunos"
         val alunos = mutableListOf("Alex", "Fran", "José", "Maria", "Anna")
-        val listaDeAlunos = findViewById<ListView>(R.id.activity_main_lista_de_alunos)
+        val listaDeAlunos = findViewById<ListView>(R.id.activity_lista_alunos_listview)
         listaDeAlunos.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos)
 
     }
