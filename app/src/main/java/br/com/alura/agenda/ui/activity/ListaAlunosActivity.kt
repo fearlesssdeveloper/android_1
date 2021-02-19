b64d635f84403f03d09059a0c705770e6a476e49
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-19 15:27:24 -0300

    android_2 - 01 - Encontrando erros com o logcat

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.kt
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.kt
@@ -2,20 +2,28 @@ package br.com.alura.agenda.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.agenda.R
+import br.com.alura.agenda.dao.AlunoDAO
 import br.com.alura.agenda.dao.AlunoDAO.Companion.alunos
 import br.com.alura.agenda.model.Aluno
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListaAlunosActivity : AppCompatActivity() {
+
+    val dao = AlunoDAO()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_alunos)
         title = "Lista de Alunos"
         configuraFabNovoAluno()
+        dao.salva(Aluno("José", "111.111.111-11", "ze_harlan@me.com"))
+        dao.salva(Aluno("Anna", "111.111.111-11", "anna@me.com"))
     }
 
     private fun configuraFabNovoAluno() {
@@ -39,5 +47,8 @@ class ListaAlunosActivity : AppCompatActivity() {
         val listaDeAlunos = findViewById<ListView>(R.id.activity_lista_alunos_listview)
         listaDeAlunos.adapter =
             ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos)
+        listaDeAlunos.setOnItemClickListener { adapterView, view, posicao, id ->
+            Log.i("posição aluno", "configuraLista: $posicao")
+        }
     }
 }
